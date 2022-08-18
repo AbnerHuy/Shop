d5a29bd8af96ea68233856de41f9c9ba92fe0595
Tran Đưc Huy <huytran.1912199&@gmail.com>
2022-08-18 20:32:39 +0700

    update reponsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,11 @@ import {
   Room,
   Twitter,
 } from "@material-ui/icons";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   display: flex;
+  ${mobile({ flexDirection: "column" })}
 `;
 const Left = styled.div`
   flex: 1;
@@ -43,6 +45,7 @@ const SocialIcon = styled.div`
 const Center = styled.div`
   flex: 1;
   padding: 20px;
+  ${mobile({ display: "none" })}
 `;
 
 const Title = styled.h3`
@@ -62,6 +65,10 @@ const ListItem = styled.li`
 
 const Right = styled.div`
   flex: 1;
+  ${mobile({
+    backgroundColor: "#fff8f8",
+    paddingLeft: "20px",
+  })}
 `;
 
 const ContactItem = styled.div`
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import { Badge, Input } from "@material-ui/core";
+import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: "50px" })}
 `;
 
 const Wrapper = styled.div`
@@ -12,6 +14,7 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  ${mobile({ padding: "10px 0" })}
 `;
 
 const Left = styled.div`
@@ -23,6 +26,7 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: "none" })}
 `;
 
 const SearchContainer = styled.div`
@@ -33,6 +37,11 @@ const SearchContainer = styled.div`
   padding: 5px;
 `;
 
+const Input = styled.input`
+  border: none;
+  ${mobile({ width: "50px" })}
+`;
+
 const Center = styled.div`
   flex: 1;
   text-align: center;
@@ -40,6 +49,7 @@ const Center = styled.div`
 
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
 `;
 
 const Right = styled.div`
@@ -47,12 +57,17 @@ const Right = styled.div`
   display: flex;
   align-item: center;
   justify-content: flex-end;
+  ${mobile({
+    flex: "2",
+    justifyContent: "center",
+  })}
 `;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
 function navbar() {
   return (
